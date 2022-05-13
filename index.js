9aa8347831bdbe978caec9cf02ceffcb65da8b70
ShareefRahat <imr4h47@gmail.com>
2022-05-13 22:01:26 +0600

    mongodb is connected with serives api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +10,32 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+//-----------MongoDB API-------------\\
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9cny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Database: MongDB is connected");
+    const serviceCollection = client.db("CavityCare").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 //-----------Node Server API----------\\
 
