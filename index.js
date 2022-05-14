1c7cd3041bd55c8fafa6e2122efcc7dcc00fcd63
ShareefRahat <imr4h47@gmail.com>
2022-05-14 11:50:19 +0600

    query for same booking for same user added

--- index.js
+++ index.js
@@ -37,8 +37,13 @@ async function run() {
       res.send(services);
     });
 
-    app.post("bookings", async (req, res) => {
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.date,
+      };
       const result = await bookingCollection.insertOne(booking);
       res.send(result);
     });
