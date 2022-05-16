5e0fbf8e668af6205e7a7cbf2e4a331bbcb08f23
ShareefRahat <imr4h47@gmail.com>
2022-05-16 19:39:35 +0600

    jwt verification function and res http code added

--- index.js
+++ index.js
@@ -23,6 +23,23 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req?.headers?.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -57,10 +74,11 @@ async function run() {
       res.send({ result, token });
     });
 
-    app.get("/bookings", async (req, res) => {
-      const patient = req.query.patient;
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const patient = req.query?.patient;
       const query = { patient: patient };
       const bookings = await bookingCollection.find(query).toArray();
+
       res.send(bookings);
     });
 
