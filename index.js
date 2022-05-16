3df00c1b85aa974e5aa9fa150f33400c0cd040a5
ShareefRahat <imr4h47@gmail.com>
2022-05-16 21:00:00 +0600

    api for getting all users is added

--- index.js
+++ index.js
@@ -55,6 +55,11 @@ async function run() {
       res.send(services);
     });
 
+    app.get("/allUsers", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
     app.put("/user/:email", async (req, res) => {
       const email = req.params?.email;
       const user = req.body;
